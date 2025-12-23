91a320d8c1d74c62bae42ee64ae515bf1f44c3bc
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-12-23 17:14:48 +0000

    resolucao total da ficha

--- fichas/ficha4.hs
+++ fichas/ficha4.hs
@@ -55,7 +55,7 @@ isZero x
 
 
 nzp :: [Int] -> (Int,Int,Int)
-nzp [] = ([],[],[])
+nzp [] = (0,0,0)
 nzp (h:t)
     | isZero h = (1+zer,pos,neg) 
     | isPos h = (zer,1+pos,neg)
@@ -66,32 +66,43 @@ nzp (h:t)
 
 
 nzp2 :: [Int] -> (Int,Int,Int)
-nzp2 is = (lenght [i | i <- is, i < 0], length [i | i <- is, i == 0], length [i | i <- is, i > 0]) 
+nzp2 is = (length [i | i <- is, i < 0], length [i | i <- is, i == 0], length [i | i <- is, i > 0]) 
 
 {-
 3. Defina a fun¸c˜ao divMod :: Integral a => a -> a -> (a, a) que calcula simultane-
 amente a divis˜ao e o resto da divis˜ao inteira por subtrac¸c˜oes sucessivas.
 -}
 
-divMod :: Integral a => a -> a -> (a, a)
-divMod = 
+divMod2 :: Integral a => a -> a -> (a, a)
+divMod2 d1 d2
+    | d1 < d2   = (0, d1)
+    | otherwise = (q + 1, r)
+  where
+    (q, r) = divMod2 (d1 - d2) d2
+
 
 {-
 4. Utilizando uma fun¸c˜ao auxiliar com um acumulador, optimize seguinte defini¸c˜ao recur-
 siva que determina qual o n´umero que corresponde a uma lista de digitos.
+-}
 
 fromDigits :: [Int] -> Int
 fromDigits [] = 0
 fromDigits (h:t) = h*10^(length t) + fromDigits t
 
+{-
 Note que:
 fromDigits [1,2,3,4] = 1 × 103 + 2 × 102 + 3 × 101 + 4 × 100 
                      = 4 + 10 × (3 + 10 × (2 + 10 × (1 + 10 × 0)))
 -}
 
 fromDigits2 :: [Int] -> Int
---fromDigits2 l = fromDigitsAux l 0
-fromDigits2 l = undefined
+fromDigits2 l = aux l 0
+    where 
+        aux [] acc = acc
+        aux (h:t) acc = aux t (acc*10 + h)
+
+
 
 {-
 5. Utilizando uma fun¸c˜ao auxiliar com acumuladores, optimize seguinte defini¸c˜ao que
@@ -101,9 +112,12 @@ maxSumInit l = maximum [sum m | m <- inits l]
 -}
 
 maxSumInit2 :: (Num a, Ord a) => [a] -> a
-maxSumInit2 l = undefined
+maxSumInit2 l = aux l 0
+    where
+        aux [] acc = acc
+        aux (h:t) acc = aux t (acc + h)
 
-maxSumInit2aux = undefined
+
 {-
 6. Optimize a seguinte defini¸c˜ao recursiva da fun¸c˜ao que calcula o n-´esimo n´umero da
 sequˆencia de Fibonacci, usando uma fun¸c˜ao auxiliar com 2 acumuladores que represen-
@@ -117,10 +131,11 @@ fib n = fib (n-1) + fib (n-2)
 -}
 
 fib2 :: Int -> Int
-fib2 n = undefined
+fib2 n = aux n 1
+    where 
+        aux 0 acc = acc
+        aux n acc = aux (n-1) (acc * n)
 
-fib2aux :: Int -> Int 
-fib2aux = undefined
 
 {-
 7. Defina a fun¸c˜ao intToStr :: Integer -> String que converte um inteiro numa
@@ -129,7 +144,17 @@ que vai devolver no final.
 -}
 
 intToStr :: Integer -> String
-intToStr n = undefined
+intToStr n
+    | n == 0    = "0"
+    | n < 0     = '-' : intToStr (-n)
+    | otherwise = aux n ""
+  where
+    aux 0 acc = acc
+    aux x acc =
+        let d = x `mod` 10
+            c = toEnum (fromEnum '0' + fromIntegral d)
+        in aux (x `div` 10) (c : acc)
+
 
 {-
 8. Para cada uma das express˜oes seguintes, exprima por enumera¸c˜ao a lista correspon-
@@ -159,21 +184,21 @@ x = [1,1,4,4,9,9,16,16,25,25]
 
 --(a) [1,2,4,8,16,32,64,128,256,512,1024]
 
-[2^x | x <- [0..10]]
+-- [2^x | x <- [0..10]]
 
 --(b) [(1,5),(2,4),(3,3),(4,2),(5,1)]
 
-[(x, 6-x) | x <- [1..5]]
+-- [(x, 6-x) | x <- [1..5]]
 
 --(c) [[1],[1,2],[1,2,3],[1,2,3,4],[1,2,3,4,5]]
 
-[[1..n] | n <- [1..5]]
+-- [[1..n] | n <- [1..5]]
 
 --(d) [[1],[1,1],[1,1,1],[1,1,1,1],[1,1,1,1,1]]
 
-[replicate n 1 | n <- [1..5]]
+-- [replicate n 1 | n <- [1..5]]
 
 --(e) [1,2,6,24,120,720]
 
-[product [1..n] | n <- [1..6]]
+-- [product [1..n] | n <- [1..6]]
 
