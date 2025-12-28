1280ee36c8bca608f2cfa1b5343fad366f8e5e5d
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-12-28 12:00:01 +0000

    enunciado do exercicio 1 da ficha 8

--- fichas/ficha8.hs
+++ fichas/ficha8.hs
@@ -1,5 +1,43 @@
 module Ficha8 where
 
+
+data Frac = F Interger Interger
+
+{-
+(a) Defina a função normaliza :: Frac -> Frac, que dada uma fracção calcula uma
+fracção equivalente, irredutı́vel, e com o denominador positivo. Por exemplo,
+normaliza (F (-33) (-51)) deve retornar F 11 17 e normaliza (F 50 (-5))
+deve retornar F (-10) 1. Sugere-se que comece por definir primeiro a função
+mdc :: Integer -> Integer -> Integer que calcula o máximo divisor comum
+entre dois números, baseada na seguinte propriedade (atribuida a Euclides):
+mdc x y == mdc (x+y) y == mdc x (y+x)
+-}
+
+-- (b) Defina Frac como instância da classe Eq.
+
+
+-- (c) Defina Frac como instância da classe Ord.
+
+
+{-
+(d) Defina Frac como instância da classe Show, de forma a que cada fracção seja
+apresentada por (numerador/denominador).
+-}
+
+{-
+(e) Defina Frac como instância da classe Num. Relembre que a classe Num tem a
+seguinte definição
+class (Eq a, Show a) => Num a where
+(+), (*), (-) :: a -> a -> a
+negate, abs, signum :: a -> a
+fromInteger :: Integer -> a
+-}
+
+{-
+(f) Defina uma função que, dada uma fracção f e uma lista de fracções l, selecciona
+de l os elementos que são maiores do que o dobro de f.
+-}
+
 -- exercicio 2
 {-
 Relembre o tipo definido na Ficha 7 para representar express˜oes inteiras. Uma poss´ıvel
